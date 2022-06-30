0af69427c5dbdf3a94ba94194b73198febee85de
Jessy Barrette <jessy.barrette@hakai.org>
2022-06-30 17:03:09 -0400

    Add prefix to nmea parser and example jupyter notebook

--- ocean_data_parser/read/nmea.py
+++ ocean_data_parser/read/nmea.py
@@ -13,16 +13,21 @@ def _get_gps_time(self):
     """Generate pandas Timestamp object from VTG and GGA NMEA information"""
     if self.get("timestamp") is None:
         return pd.NaT
+
+    # Review time format
+    time_format = "%H%M%S" if len(self["timestamp"]) == 6 else "%H%M%S.%f"
+    # Convert to timestamp
     if self.get("datestamp"):
+
         return pd.to_datetime(
             f"{self['datestamp']}T{self['timestamp']}",
-            format="%d%m%yT%H%M%S.%f",
+            format=f"%d%m%yT{time_format}",
             utc=True,
         )
     elif self.get("year") and self.get("month") and self.get("day"):
         return pd.to_datetime(
             f"{self['year']}-{self['month']}-{self['day']} {self['timestamp']}",
-            format="%Y-%m-%d %H%M%S.%f",
+            format=f"%Y-%m-%d {time_format}",
             utc=True,
         )
 
@@ -42,18 +47,29 @@ def _get_longitude(self):
             float(self["lon"][:3]) + float(self["lon"][4:]) / 60
         )
 
+def _generate_gps_variables(df):
+    """Generate standardized variables from the different variables available"""
+    # Generate variables
+    df["nmea_type"] = df["talker"] + df["sentence_type"].fillna("manufacturer")
+    df["gps_time"] = df.apply(_get_gps_time, axis=1)
+    df["latitude_degrees_north"] = df.apply(_get_latitude, axis=1)
+    df["longitude_degrees_east"] = df.apply(_get_longitude, axis=1)
+    return df
 
-def file(path, encoding="UTF-8"):
+def file(path, encoding="UTF-8", nmea_delimiter="$"):
     """Parse a file containing NMEA information into a pandas dataframe"""
     nmea = []
     with open(path, encoding=encoding) as f:
 
-        for line in f:
+        for row, line in enumerate(f):
             try:
-                parsed_line = pynmea2.parse(line)
+                prefix, nmea_string = line.split(nmea_delimiter)
+                parsed_line = pynmea2.parse(nmea_delimiter + nmea_string)
                 parsed_items = parsed_line.__dict__
                 nmea += [
                     {
+                        "row": row,
+                        "prefix": prefix,
                         "talker": parsed_items.get("talker"),
                         "sentence_type": parsed_items.get("sentence_type"),
                         "subtype": parsed_items.get("subtype"),
@@ -74,11 +90,16 @@ def file(path, encoding="UTF-8"):
 
     # Convert NMEA to a dataframe
     df = pd.DataFrame(nmea).astype(object).replace(np.nan, None)
-    df = generate_gps_variables(df)
-    return df
+    df = _generate_gps_variables(df)
+
+    # Convert to xarray
+    ds = df.to_xarray()
+    # TODO Apply vocabulary
+
+    return ds
 
 
-def generate_gps_variables(df):
+def _generate_gps_variables(df):
     """Generate standardized variables from the different variables available"""
     # Generate variables
     df["nmea_type"] = df["talker"] + df["sentence_type"].fillna("manufacturer")
