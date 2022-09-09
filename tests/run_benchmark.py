ea61ecdb6c0986ade17c6f26251f8ed531a8c257
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2022-09-09 15:55:01 -0400

    add benchmark for onset and van_essen and speed up van_essen MON parser

--- tests/run_benchmark.py
+++ tests/run_benchmark.py
@@ -21,3 +21,23 @@ def test_benchmark_amundsen_trajectory(benchmark):
             "tests/parsers_test_files/amundsen/*trajectory/**/*.int", recursive=True
         ),
     )
+
+def test_benchmark_onset_csv(benchmark):
+    benchmark(
+        batch_parse_and_save_to_netcdf,
+        parser=read.onset.csv,
+        files=glob(
+            "tests/parsers_test_files/onset/**/*.cnv", recursive=True
+        ),
+    )
+
+
+def test_benchmark_van_essen_mon(benchmark):
+    benchmark(
+        batch_parse_and_save_to_netcdf,
+        parser=read.van_essen_instruments.mon,
+        files=glob(
+            "tests/parsers_test_files/van_essen_instruments/**/*.MON", recursive=True
+        ),
+    )
+
