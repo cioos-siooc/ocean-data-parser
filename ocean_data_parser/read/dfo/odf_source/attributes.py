3f50c0a5c73a7417d50b5995914bb3d3baae73c3
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2022-12-13 15:13:27 -0500

    return title attributes check to the original suggestion

--- ocean_data_parser/read/dfo/odf_source/attributes.py
+++ ocean_data_parser/read/dfo/odf_source/attributes.py
@@ -450,10 +450,7 @@ def global_attributes_from_header(dataset, odf_header, config=None):
     dataset.attrs.update(_get_attribute_mapping_corrections())
 
     # Generate attributes from other attributes
-    if all(
-        item in dataset.attrs.keys()
-        for item in ["organization", "institution", "platform"]
-    ):
+    if dataset.attrs.keys() >= {"organization", "institution", "platform"}:
         dataset.attrs["title"] = _generate_title_from_global_attributes(dataset.attrs)
     dataset.attrs.update(**_generate_program_specific_attritutes(dataset.attrs))
 
