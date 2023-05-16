e76d76487782efc8c87f1fc3663c14a9f8d8037a
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-05-16 15:20:48 -0400

    black .

--- ocean_data_parser/metadata/nerc.py
+++ ocean_data_parser/metadata/nerc.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 
-def get_vocabulary(vocab:str) -> pd.DataFrame:
+def get_vocabulary(vocab: str) -> pd.DataFrame:
     """Retrieve NERC vocabulary full list"""
     df = pd.read_json(
         f"http://vocab.nerc.ac.uk/collection/{vocab.upper()}/current/?_profile=dd&_mediatype=application/json"
@@ -14,7 +14,7 @@ def get_vocabulary(vocab:str) -> pd.DataFrame:
     return df
 
 
-def get_vocabulary_term(vocab:str, id:str) -> dict:
+def get_vocabulary_term(vocab: str, id: str) -> dict:
     url = f"http://vocab.nerc.ac.uk/collection/{vocab}/current/{id}/?_profile=nvs&_mediatype=application/ld+json"
     with requests.get(url) as response:
         json_text = response.text
