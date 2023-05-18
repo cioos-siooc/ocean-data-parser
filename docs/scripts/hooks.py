1097cd01441026d2d5b750e0d6ff95e0d8c7e68b
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-05-18 17:35:30 -0400

    point hooks to the version based on active branch

--- docs/scripts/hooks.py
+++ docs/scripts/hooks.py
@@ -1,13 +1,30 @@
+import logging
 import pandas as pd
 import numpy as np
 import json
+from pathlib import Path
+
+import mkdocs.plugins
+
+logger = logging.getLogger("mkdocs")
 
 
 def quote_column(col):
     return "`" + col + "`"
 
 
-def get_odf_vocab_markdown():
+def get_active_branch_name():
+
+    head_dir = Path(".") / ".git" / "HEAD"
+    with head_dir.open("r") as f:
+        content = f.read().splitlines()
+
+    for line in content:
+        if line[0:4] == "ref:":
+            return line.partition("refs/heads/")[2]
+
+
+def get_odf_vocab_markdown(version):
     df = pd.read_csv(
         "ocean_data_parser/read/dfo/odf_source/references/reference_vocabulary.csv"
     )
@@ -18,14 +35,14 @@ def get_odf_vocab_markdown():
         "apply_function",
     ]:
         df[column] = quote_column(df[column])
-    with open("docs/read/dfo/odf-hook.md", "w") as file_handle:
+    with open(f"{version}/docs/read/dfo/odf-hook.md", "w") as file_handle:
         file_handle.write(
             """:::ocean_data_parser.read.dfo.odf\n\n## ODF Vocabulary\n"""
         )
         df.replace({np.nan: ""}).to_markdown(file_handle, index=False, tablefmt="pipe")
 
 
-def get_ios_vocab_markdown():
+def get_ios_vocab_markdown(version):
     df = pd.read_csv(
         "https://raw.githubusercontent.com/cioos-siooc/cioos-siooc_data_transform/ios-parser-extra-vocabulary/cioos_data_transform/cioos_data_transform/utils/ios_vocabulary.csv"
     )
@@ -36,14 +53,14 @@ def get_ios_vocab_markdown():
         "apply_func",
     ]:
         df[column] = quote_column(df[column].astype(str))
-    with open("docs/read/dfo/ios-hook.md", "w") as file_handle:
+    with open(f"{version}/docs/read/dfo/ios-hook.md", "w") as file_handle:
         file_handle.write(
             """:::ocean_data_parser.read.dfo.ios\n\n## IOS Vocabulary\n"""
         )
         df.replace({np.nan: ""}).to_markdown(file_handle, index=False, tablefmt="pipe")
 
 
-def get_amundsen_vocab_markdown():
+def get_amundsen_vocab_markdown(version):
     with open(
         "ocean_data_parser/read/vocabularies/amundsen_variable_attributes.json"
     ) as file_handle:
@@ -56,12 +73,12 @@ def get_amundsen_vocab_markdown():
             if attrs and name != "VARIABLE_NAME"
         ]
     )
-    with open("docs/read/amundsen-hook.md", "w") as file_handle:
+    with open(f"{version}/docs/read/amundsen-hook.md", "w") as file_handle:
         file_handle.write(""":::ocean_data_parser.read.amundsen\n\n## Vocabulary\n""")
         df.replace({np.nan: ""}).to_markdown(file_handle, index=False, tablefmt="pipe")
 
 
-def get_seabird_vocab_markdown():
+def get_seabird_vocab_markdown(version):
     with open(
         "ocean_data_parser/read/vocabularies/seabird_variable_attributes.json"
     ) as file_handle:
@@ -72,13 +89,20 @@ def get_seabird_vocab_markdown():
             for name, attrs in vocab.items()
         ]
     )
-    with open("docs/read/seabird-hook.md", "w") as file_handle:
+    with open(f"{version}/docs/read/seabird-hook.md", "w") as file_handle:
         file_handle.write(""":::ocean_data_parser.read.seabird\n\n## Vocabulary\n""")
         df.replace({np.nan: ""}).to_markdown(file_handle, index=False, tablefmt="pipe")
 
 
 def on_pre_build(config, **kwargs) -> None:
-    get_odf_vocab_markdown()
-    get_ios_vocab_markdown()
-    get_amundsen_vocab_markdown()
-    get_seabird_vocab_markdown()
+    logger.debug("config=%s", config)
+    logger.debug("kwargs=%s", kwargs)
+
+    logger.info("Active Branch=%s", get_active_branch_name())
+    active_branch = get_active_branch_name()
+    logger.info("Active Branch=%s", active_branch)
+    version = "main" if active_branch == "main" else "dev"
+    get_odf_vocab_markdown(version)
+    get_ios_vocab_markdown(version)
+    get_amundsen_vocab_markdown(version)
+    get_seabird_vocab_markdown(version)
