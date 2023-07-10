3c1cc1299c8636f3b1a6ae47bcd9b84cc6a182bf
Jessy Barrette <jessy.barrette@hakai.org>
2023-07-10 15:02:34 -0400

    improve generate_output_path

--- ocean_data_parser/batch/utils.py
+++ ocean_data_parser/batch/utils.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 
 import xarray
+import pandas as pd
 
 
-def _generate_output_path(
+def generate_output_path(
     ds: xarray.Dataset,
-    path: str,
     source: str = None,
+    path: str = ".",
     defaults: dict = None,
     file_preffix: str = "",
     file_suffix: str = "",
-    output_format: str = None,
+    output_format: str = ".nc",
 ) -> Path:
     """Generate output path where to save Dataset.
 
@@ -21,9 +22,9 @@ def _generate_output_path(
             attributes accoding to the convention:
               - source_filename: pathlib.Path of original parsed file filename
               - source_filename_stem: original parsed file filename without the extension
-              - global attributes: `global_{Attribute}`
-              - variable attributes: `variable_{variable}_{attribute}`
-            ex: ".\{global_program}\{global_project}\{source_filename.name}.nc"
+              - global attributes: `global:{Attribute}`
+              - variable attributes: `variable:{variable}:{attribute}`
+            ex: ".\{global:program}\{global:project}\{source_filename.name}.nc"
         source (str, optional): original source file path. Defaults to None.
         defaults (dict, optional): Placeholder for any global
             attributes or variable attributes used in output path. Defaults to None.
@@ -32,43 +33,49 @@ def _generate_output_path(
         output_format (str, optional): Output File Format extension.
 
     Returns:
-        Path: _description_
+        Path (Path): Generated path
     """
 
-    def _add_preffix_suffix(filename: Path):
-        return Path(filename.parent) / (
-            (file_preffix or "") + filename.stem + (file_suffix or "") + filename.suffix
-        )
+    if source is None and ds.attrs.get("source"):
+        source = Path(ds.attrs["source"]).stem
 
-    output_format = output_format or Path(path or ".").suffix
+    if source is None:
+        raise RuntimeError("No output source available. Please define source output.")
+
+    # Review file_output path given by config
+    path_generation_inputs = {
+        "source_filename": source or ".",
+        **(defaults or {}),
+        **{f"{key}": value for key, value in ds.attrs.items() if value},
+        **{
+            f"variable_{var}_{key}": value
+            for var in ds.variables
+            for key, value in ds[var].attrs.items()
+            if value
+        },
+        **(
+            {
+                "time_min": pd.to_datetime(ds["time"].min().values),
+                "time_max": pd.to_datetime(ds["time"].max().values),
+            }
+            if "time" in ds
+            else {}
+        ),
+    }
+
+    # Generate path and file name
+    output_path = Path(path.format(**path_generation_inputs))
+    source = source.format(**path_generation_inputs)
+
+    # Retrieve output_format if given in source
+
+    if "." in source and not output_format:
+        source, output_format = source.rsplit(".", 1)
     assert (
         output_format
     ), "Unknown output file format extension: define the format through the path or output_format inputs"
 
-    if path is None and source:
-        return _add_preffix_suffix(Path(f"{source}{output_format}"))
-
-    defaults = defaults or {}
-    # Review file_output path given by config
-    path_generation_inputs = {
-        "source_filename": Path(source or "."),
-        **defaults,
-        **{f"global_{key}": value for key, value in ds.attrs.items()},
-        **{
-            f"variable_{var}_{key}": value
-            for var in ds
-            for key, value in ds[var].attrs.items()
-        },
-        **{
-            f"variable_{var}_{key}": value
-            for var in ds.coords
-            for key, value in ds[var].attrs.items()
-        },
-    }
-
-    output_path = Path(path.format(**path_generation_inputs))
-    if output_path.suffix != output_format:
-        output_path += output_format
-    if not output_path.name:
-        output_path = output_path / Path(source).stem + output_format
-    return _add_preffix_suffix(output_path)
+    # Generate path
+    return Path(output_path) / (
+        f"{file_preffix or ''}{source}{file_suffix or ''}{output_format}"
+    )
--- tests/test_batch.py
+++ tests/test_batch.py
@@ -2,19 +2,19 @@ import logging
 import logging.config
 import unittest
 from pathlib import Path
-from time import sleep
 
+import xarray as xr
 import pandas as pd
 from click.testing import CliRunner
-from utils import compare_text_files
+import pytest
 
-from ocean_data_parser.batch import convert
 from ocean_data_parser.batch.convert import (
     FileConversionRegistry,
     cli_files,
     load_config,
     main,
 )
+from ocean_data_parser.batch.utils import generate_output_path
 
 PACKAGE_PATH = Path(__file__).parent
 TEST_REGISTRY_PATH = Path("tests/test_file_registry.csv")
@@ -38,11 +38,22 @@ class ConfigLoadTests(unittest.TestCase):
 
 
 class BatchModeTests(unittest.TestCase):
-    def test_batch_conversion_onset_parser(self):
+    def test_batch_conversion_onset_parser_single_runner(self):
         config = load_config()
         config["input_path"] = "tests/parsers_test_files/onset/**/*.csv"
         config["parser"] = "onset.csv"
         config["overwrite"] = True
+        config["multiprocessing"] = None
+        config["file_output"]["path"] = "temp/batch/single_files/"
+        main(config=config)
+
+    def test_batch_conversion_onset_parser_multiprocessing(self):
+        config = load_config()
+        config["input_path"] = "tests/parsers_test_files/onset/**/*.csv"
+        config["parser"] = "onset.csv"
+        config["overwrite"] = True
+        config["multiprocessing"] = 3
+        config["file_output"]["path"] = "temp/batch/multiprocessing_files/"
         main(config=config)
 
     def test_batch_cli_conversion_onset_parser(self):
@@ -88,3 +99,56 @@ def get_test_file_registry():
     test_file.touch()
     return test_file_registry, test_file
 
+
+test_ds = xr.Dataset()
+test_ds.attrs["organization"] = "organization"
+test_ds.attrs["instrument"] = "InstrumentName"
+test_ds.attrs["instrument_serial_number"] = "64651354"
+test_ds["time"] = pd.to_datetime(
+    pd.Series(["2022-01-01T00:00:00Z", "2022-03-02T00:00:00Z"])
+)
+test_ds["time"].attrs["timezone"] = "UTC"
+
+
+class TestBatchGenerateName:
+    def test_generate_default_name(self):
+        name = generate_output_path(test_ds)
+        assert isinstance(name, Path)
+
+    def test_generate_output_from_source_attribute(self):
+        source_ds = test_ds.copy()
+        source_ds.attrs["source"] = "source_file.csv"
+        name = generate_output_path(source_ds)
+        assert isinstance(name, Path)
+        assert str(name) == "source_file.nc"
+
+    def test_generate_filename_with_path(self):
+        name = generate_output_path(
+            test_ds, source="{organization}_{instrument}_test", output_format=".nc"
+        )
+        assert isinstance(name, Path)
+        assert str(name) == "organization_InstrumentName_test.nc"
+
+    def test_generate_filename_with_time(self):
+        name = generate_output_path(
+            test_ds,
+            source="{organization}_{instrument}_{time_min:%Y%m%d}-{time_max:%Y%m%d}",
+            output_format=".nc",
+        )
+        assert isinstance(name, Path)
+        assert str(name) == "organization_InstrumentName_20220101-20220302.nc"
+
+    def test_generate_filename_with_variable_attribute(self):
+        name = generate_output_path(
+            test_ds,
+            source="{organization}_{instrument}_{variable_time_timezone}",
+            output_format=".nc",
+        )
+        assert isinstance(name, Path)
+        assert str(name) == "organization_InstrumentName_UTC.nc"
+
+    def test_generate_filename_with_missing_source(self):
+        fail_ds = test_ds.copy()
+        fail_ds.attrs["source"] = None
+        with pytest.raises(Exception) as e_info:
+            name = generate_output_path(fail_ds)
