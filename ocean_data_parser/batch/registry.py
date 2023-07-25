08ee758927c73c9b917e994857eff57745397b14
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-07-25 15:54:04 -0400

    add type hints to registry module

--- ocean_data_parser/batch/registry.py
+++ ocean_data_parser/batch/registry.py
@@ -70,7 +70,7 @@ class FileConversionRegistry:
     def deepcopy(self):
         return copy.deepcopy(self)
 
-    def _get_hash(self, file):
+    def _get_hash(self, file) -> str:
 
         file = Path(file)
         if not file.exists():
@@ -146,7 +146,7 @@ class FileConversionRegistry:
             .last()
         )
 
-    def _get_sources(self, sources: list):
+    def _get_sources(self, sources: list) -> list:
         if isinstance(sources, list):
             return sources
         return self.data.index.to_list()
@@ -160,20 +160,23 @@ class FileConversionRegistry:
         )
         return is_different
 
-    def _is_different_mtime(self):
+    def _is_different_mtime(self) -> pd.Series:
         return self.data["last_update"] != self.data.index.map(self._get_mtime)
 
-    def _is_modified_since(self):
+    def _is_modified_since(self) -> pd.Series:
         since = self._get_since_timestamp()
         return self.data.index.to_series().apply(self._get_mtime) - since >= 0
+    
+    def _is_new_file(self) -> pd.Series:
+        return ~self._output_file_exists() & self._has_no_error()
 
-    def _source_exist(self):
+    def _source_exist(self) -> pd.Series:
         return self.data.index.to_series().apply(self._file_exists)
 
-    def _output_file_exists(self):
+    def _output_file_exists(self) -> pd.Series:
         return self.data["output_path"].apply(self._file_exists)
 
-    def _has_no_error(self):
+    def _has_no_error(self) -> pd.Series:
         return self.data["error_message"].isna()
 
     def update(self, sources: list = None):
@@ -186,9 +189,8 @@ class FileConversionRegistry:
         sources = self._get_sources(sources)
         self.data.loc[sources, "hash"] = list(map(self._get_hash, sources))
         self.data.loc[sources, "last_update"] = list(map(self._get_mtime, sources))
-        return
 
-    def update_fields(self, sources=None, **kwargs):
+    def update_fields(self, sources:list =None, **kwargs):
         """Update given sources specific fields in attributes
 
         Args:
@@ -200,7 +202,7 @@ class FileConversionRegistry:
                 self.data[field] = None
             self.data.loc[sources, field] = value
 
-    def get_source_files_to_parse(self, overwrite=True):
+    def get_source_files_to_parse(self, overwrite:bool=True) -> list:
         is_new = ~self._output_file_exists() & self._has_no_error()
         if not overwrite:
             return self.data.loc[is_new].index.to_list()
@@ -212,7 +214,7 @@ class FileConversionRegistry:
 
         return self.data.loc[is_new | is_modified].index.to_list()
 
-    def get_missing_sources(self):
+    def get_missing_sources(self) -> list:
         """Get list of missing sources
 
         Returns:
