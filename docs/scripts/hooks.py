6371a574720fce8ce9a699a8ebb1acc91fec9f9c
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-07-28 15:38:02 -0400

    add a hook to copy notebooks to docs/notebooks

--- docs/scripts/hooks.py
+++ docs/scripts/hooks.py
@@ -1,4 +1,6 @@
 import json
+from pathlib import Path
+import shutil
 
 import numpy as np
 import pandas as pd
@@ -10,19 +12,19 @@ def quote_column(col):
 
 def get_dfo_pfile_vocab_markdown():
     """Convert P file vocabulary to markdown table"""
-    df = pd.read_csv("ocean_data_parser/read/vocabularies/dfo_p_files_vocabulary.csv")
+    df = pd.read_csv("ocean_data_parser/parsers/vocabularies/dfo_p_files_vocabulary.csv")
     for column in ["accepted_instruments"]:
         df[column] = quote_column(df[column])
-    with open("docs/read/dfo/p-header.md") as file_handle:
+    with open("docs/parsers/dfo/p-header.md") as file_handle:
         header = file_handle.read()
-    with open("docs/read/dfo/p-hook.md", "w") as file_handle:
+    with open("docs/parsers/dfo/p-hook.md", "w") as file_handle:
         file_handle.write(header)
         df.replace({np.nan: ""}).to_markdown(file_handle, index=False, tablefmt="pipe")
 
 
 def get_odf_vocab_markdown():
     df = pd.read_csv(
-        "ocean_data_parser/read/dfo/odf_source/references/reference_vocabulary.csv"
+        "ocean_data_parser/parsers/dfo/odf_source/references/reference_vocabulary.csv"
     )
     for column in [
         "accepted_units",
@@ -31,9 +33,9 @@ def get_odf_vocab_markdown():
         "apply_function",
     ]:
         df[column] = quote_column(df[column])
-    with open("docs/read/dfo/odf-header.md") as file_handle:
+    with open("docs/parsers/dfo/odf-header.md") as file_handle:
         header = file_handle.read()
-    with open("docs/read/dfo/odf-hook.md", "w") as file_handle:
+    with open("docs/parsers/dfo/odf-hook.md", "w") as file_handle:
         file_handle.write(header)
         df.replace({np.nan: ""}).to_markdown(file_handle, index=False, tablefmt="pipe")
 
@@ -49,16 +51,16 @@ def get_ios_vocab_markdown():
         "apply_func",
     ]:
         df[column] = quote_column(df[column].astype(str))
-    with open("docs/read/dfo/ios-header.md") as file_handle:
+    with open("docs/parsers/dfo/ios-header.md") as file_handle:
         header = file_handle.read()
-    with open("docs/read/dfo/ios-hook.md", "w") as file_handle:
+    with open("docs/parsers/dfo/ios-hook.md", "w") as file_handle:
         file_handle.write(header)
         df.replace({np.nan: ""}).to_markdown(file_handle, index=False, tablefmt="pipe")
 
 
 def get_amundsen_vocab_markdown():
     with open(
-        "ocean_data_parser/read/vocabularies/amundsen_variable_attributes.json"
+        "ocean_data_parser/parsers/vocabularies/amundsen_variable_attributes.json"
     ) as file_handle:
         vocab = json.load(file_handle)
     df = pd.DataFrame(
@@ -69,7 +71,7 @@ def get_amundsen_vocab_markdown():
             if attrs and name != "VARIABLE_NAME"
         ]
     )
-    with open("docs/read/amundsen-hook.md", "w") as file_handle:
+    with open("docs/parsers/amundsen-hook.md", "w") as file_handle:
         file_handle.write(
             """:::ocean_data_parser.parsers.amundsen\n\n## Vocabulary\n"""
         )
@@ -78,7 +80,7 @@ def get_amundsen_vocab_markdown():
 
 def get_seabird_vocab_markdown():
     with open(
-        "ocean_data_parser/read/vocabularies/seabird_variable_attributes.json"
+        "ocean_data_parser/parsers/vocabularies/seabird_variable_attributes.json"
     ) as file_handle:
         vocab = json.load(file_handle)
     df = pd.DataFrame(
@@ -87,9 +89,17 @@ def get_seabird_vocab_markdown():
             for name, attrs in vocab.items()
         ]
     )
-    with open("docs/read/seabird-hook.md", "w") as file_handle:
+    with open("docs/parsers/seabird-hook.md", "w") as file_handle:
         file_handle.write(""":::ocean_data_parser.parsers.seabird\n\n## Vocabulary\n""")
         df.replace({np.nan: ""}).to_markdown(file_handle, index=False, tablefmt="pipe")
+
+
+def copy_notebooks():
+    """Copy notebooks made available within the notebook forlder"""
+    notebooks = Path('notebooks').glob('*.ipynb')
+    for notebook in notebooks:
+        shutil.copy(notebook,Path('docs/notebooks') / notebook.name)
+
 
 
 def on_pre_build(config, **kwargs) -> None:
@@ -98,3 +108,4 @@ def on_pre_build(config, **kwargs) -> None:
     get_amundsen_vocab_markdown()
     get_seabird_vocab_markdown()
     get_dfo_pfile_vocab_markdown()
+    copy_notebooks()
