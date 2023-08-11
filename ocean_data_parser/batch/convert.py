5ffc041d94b0623dbbaedfb46808d3cd4ce04de2
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-08-11 15:45:51 -0400

    move logger parameters to main cli command and add inputs to it

--- ocean_data_parser/batch/convert.py
+++ ocean_data_parser/batch/convert.py
@@ -1,4 +1,3 @@
-import logging
 import os
 import shutil
 import sys
@@ -8,72 +7,19 @@ from pathlib import Path
 
 import click
 import pandas as pd
-from dotenv import load_dotenv
 from loguru import logger
 from tqdm import tqdm
 from xarray import Dataset
 
-from ocean_data_parser import geo, process, read
+from ocean_data_parser import PARSERS, geo, process, read
 from ocean_data_parser._version import __version__
 from ocean_data_parser.batch.config import load_config
 from ocean_data_parser.batch.registry import FileConversionRegistry
 from ocean_data_parser.batch.utils import VariableLevelLogger, generate_output_path
 from ocean_data_parser.parsers import utils
-from ocean_data_parser import PARSERS
 
 MODULE_PATH = Path(__file__).parent
 DEFAULT_CONFIG_PATH = MODULE_PATH / "default-batch-config.yaml"
-
-# Set logging configuration
-load_dotenv(".env")
-logger.configure(extra={"source_file": ""})
-logger.remove()
-logger.add(
-    sys.stderr,
-    level=os.getenv("LOGURU_LEVEL", "INFO"),
-    format=(
-        '<level>{level.icon}</level> <blue>"{file.path}"</blue>: '
-        '<yellow>line {line}</yellow> | <cyan>"{extra[source_file]}"</cyan> - '
-        "<level>{message}</level>"
-    ),
-)
-logger.add(
-    "ocean_data_parser.log",
-    level=os.getenv("LOGURU_LEVEL", "WARNING"),
-    format="{time}|{level}|{file.path}:{line}| {extra[source_file]} - {message}",
-)
-
-
-# Redirect logging loggers to loguru
-class InterceptHandler(logging.Handler):
-    """
-    This class is used to redirect logging loggers to loguru
-    # https://stackoverflow.com/questions/66769431/how-to-use-loguru-with-standard-loggers
-    """
-
-    @logger.catch(default=True, onerror=lambda _: sys.exit(1))
-    def emit(self, record):
-        # Get corresponding Loguru level if it exists.
-        try:
-            level = logger.level(record.levelname).name
-        except ValueError:
-            level = record.levelno
-
-        # Find caller from where originated the logged message.
-        frame, depth = sys._getframe(6), 6
-        while frame and frame.f_code.co_filename == logging.__file__:
-            frame = frame.f_back
-            depth += 1
-
-        logger.opt(depth=depth, exception=record.exc_info).log(
-            level, record.getMessage()
-        )
-
-
-logging.basicConfig(
-    handlers=[InterceptHandler()], level=os.getenv("LOGURU_LEVEL", "INFO")
-)
-classic_logger = logging.getLogger()
 
 
 def save_new_config(ctx, _, path):
--- ocean_data_parser/cli.py
+++ ocean_data_parser/cli.py
@@ -1,14 +1,90 @@
+import logging
+import os
+import sys
+
 import click
+from loguru import logger
 
 from ocean_data_parser import __version__
-
 from ocean_data_parser.batch.convert import convert as convert
 from ocean_data_parser.compile.netcdf import compile as compile
 
-@click.group(name='odpy')
+LOG_LEVELS = ["TRACE", "DEBUG", "INFO", "WARNING", "ERROR"]
+VERBOSE_LOG_FORMAT = (
+    '<level>{level.icon}</level> <blue>"{file.path}"</blue>: '
+    '<yellow>line {line}</yellow> | <cyan>"{extra[source_file]}"</cyan> - '
+    "<level>{message}</level>"
+)
+LOG_FORMAT = (
+    '<level>{level.icon}</level> <cyan>"{extra[source_file]}"</cyan> - '
+    "<level>{message}</level>"
+)
+logger.remove()
+logger.configure(extra={"source_file": ""})
+
+# Redirect logging loggers to loguru
+class InterceptHandler(logging.Handler):
+    """
+    This class is used to redirect logging loggers to loguru
+    # https://stackoverflow.com/questions/66769431/how-to-use-loguru-with-standard-loggers
+    """
+
+    @logger.catch(default=True, onerror=lambda _: sys.exit(1))
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists.
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message.
+        frame, depth = sys._getframe(6), 6
+        while frame and frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+logging.basicConfig(
+    handlers=[InterceptHandler()], level=os.getenv("LOGURU_LEVEL", "INFO")
+)
+classic_logger = logging.getLogger()
+
+
+@click.group(name="odpy")
 @click.version_option(__version__)
-def main():
-    pass
+@click.option(
+    "--verbose",
+    is_flag=True,
+    help=f"Logger format present more information",
+    default=False,
+)
+@click.option(
+    "--log-level",
+    type=click.Choice(LOG_LEVELS),
+    help="Logger level used",
+    default="INFO",
+)
+@click.option("--log-file", type=click.Path(), help="Log output to a file.")
+@click.option(
+    "--log-file-level",
+    type=click.Choice(LOG_LEVELS),
+    help="File ogger level used",
+    default="INFO",
+)
+def main(verbose, log_level, log_file, log_file_level):
+    log_format = VERBOSE_LOG_FORMAT if verbose else LOG_FORMAT
+    logger.add(
+        sys.stderr,
+        level=log_level,
+        format=VERBOSE_LOG_FORMAT if verbose else LOG_FORMAT,
+    )
+    if log_file:
+        logger.add(log_file, level=log_file_level, format=log_format)
+
 
 main.add_command(convert)
 main.add_command(compile)
