0407c911e259a5d7068de093957efa7574c39e20
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-08-12 07:53:07 -0400

    add multiprocessing option to odpy.compile and improve logging

--- ocean_data_parser/compile/netcdf.py
+++ ocean_data_parser/compile/netcdf.py
@@ -1,12 +1,12 @@
-from pathlib import Path
+import os
 from glob import glob
+from multiprocessing import Pool
 
 import click
 import pandas as pd
 import xarray as xr
-from tqdm import tqdm
 from loguru import logger
-
+from tqdm import tqdm
 
 
 def _get_erddap_dtype(dtype):
@@ -42,6 +42,7 @@ def _get_netcdf_variables(file) -> list:
         return dict(
             file=file, variable=var, dtype=ds[var].dtype, **ds[var].attrs, **kwargs
         )
+
     logger.contextualize(source_file=file)
     ds = xr.open_dataset(file)
     variables = [_get_var_attrs(var, coords=True) for var in ds.coords]
@@ -55,6 +56,7 @@ def variables(
     groupby: str = "variable,units,standard_name,sdn_parameter_urn,sdn_parameter_name",
     output_table: str = None,
     output_erddap_xml: str = None,
+    multiprocessing: int = None,
 ):
     """Compile NetCDF files variables and variables attributes.
 
@@ -72,7 +74,7 @@ def variables(
         output_erddap_xml (str, optional): Path to where to ouput
             erddap dataset xml. Defaults to console.
     """
-    
+
     # Get file list
     logger.debug("Retrieve files to compile")
     files = glob(input, recursive=True)
@@ -81,42 +83,49 @@ def variables(
         logger.debug("Retrieve files to ignore")
         excluded_files = glob(exclude, recursive=True)
         files = [file for file in files if file not in excluded_files]
-    
+
     if not files:
         logger.info("No netcdf files available")
         return
 
     # Compile variable list
-    variables = []
-    for file in tqdm(files, unit="file", desc="Compile NetCDF variables"):
-        variables += _get_netcdf_variables(file)
-
+    tqdm_kwargs = dict(unit="file", desc="Compile NetCDF variables", total=len(files))
+    if multiprocessing == 1:
+        variables = [_get_netcdf_variables(file) for file in tqdm(files, **tqdm_kwargs)]
+    else:
+        with Pool(multiprocessing) as pool:
+            variables = list(
+                tqdm(pool.imap(_get_netcdf_variables, files), **tqdm_kwargs)
+            )
     # Generate DataFrame
-    df_vars = pd.DataFrame(variables)
-    logger.debug("columns available: %s", df_vars.columns)
+    #  unpack variables which is a list of list of variables per files.
+    df_vars = pd.DataFrame(sum(variables, []))
     df_vars_grouped = (
         df_vars.groupby(
-            groupby.split(','),
+            groupby.split(","),
             dropna=False,
         ).agg({"file": "count"})
     ).reset_index()
     df_vars_unique = df_vars.drop_duplicates(subset=["variable", "units"], keep="first")
 
     # Generate output
-    logger.info("Unique Variable list")
     if output_table is None:
+        logger.info("Generate variable tables:")
         print(df_vars_grouped.to_markdown())
     elif output_table.endswith("md"):
+        logger.info("Save variable table to: {}", output_table)
         df_vars_grouped.to_markdown(output_table)
     elif output_table.endswith("csv"):
+        logger.info("Save variable table to: {}", output_table)
         df_vars_grouped.to_csv(output_table)
 
     # Gernerate
     if output_erddap_xml:
+        logger.debug("Compile xml")
         erddap_xml = []
-        for id, row in df_vars_unique.iterrows():
+        for _, row in df_vars_unique.iterrows():
             attrs = row.dropna().to_dict()
-            file = attrs.pop("file")
+            attrs.pop("file")
             var = attrs.pop("variable")
             dtype = _get_erddap_dtype(attrs.pop("dtype", "String"))
             erddap_xml += [_get_erddap_xml_variable(var, var, dtype, attrs)]
@@ -137,22 +146,30 @@ def variables(
     type=str,
     help="Glob expression of input files",
 )
-@click.option(
-    "--exclude", type=str, help="Glob expression of files to exclude"
-)
+@click.option("--exclude", type=str, help="Glob expression of files to exclude")
 @click.option(
     "--groupby",
     default="variable,units,standard_name,sdn_parameter_urn,sdn_parameter_name",
     type=str,
     help="Comma separated list of attributes to regroup variables by.",
-    show_default=True
+    show_default=True,
+)
+@click.option(
+    "--multiprocessing",
+    "-m",
+    type=click.IntRange(1, os.cpu_count()),
+    default=1,
+    flag_value=os.cpu_count(),
+    is_flag=False,
+    help=f"Load files in parallele with n processors. If the option is set as a flag, all the processors available (={os.cpu_count()}) will be used.",
+    show_default=True,
 )
 @click.option(
     "--output_table",
     "-t",
     default=None,
     is_flag=False,
-    flag_value='odpy-compile-variables.md',
+    flag_value="odpy-compile-variables.md",
     help="Result table output: \n - None = console(default)\n - path to csv or markdown(*.md) file.",
 )
 @click.option(
@@ -160,7 +177,7 @@ def variables(
     "--xml",
     default=None,
     is_flag=False,
-    flag_value='odpy-compile-variables.xml',
+    flag_value=True,
     help="Output an ERDDAP XML blurb or all the variables. ",
 )
 def compile(**kwargs):
