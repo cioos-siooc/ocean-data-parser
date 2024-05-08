a4ba656dd7cc5e13f058d9e1de2876186e0d1431
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-05-08 16:18:22 -0400

    add a few tests for platform vocabulary

--- tests/test_vocabularies.py
+++ tests/test_vocabularies.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import pytest
 
 from ocean_data_parser.vocabularies import load
+
+
+@pytest.fixture(scope="module")
+def platforms_vocab():
+    return load.dfo_platforms()
 
 
 def test_amundsen_vocabulary_load():
@@ -15,12 +21,55 @@ def test_seabird_vocabulary_load():
     assert isinstance(vocab, dict)
 
 
-def test_dfo_platform_load():
-    vocab = load.dfo_platforms()
-    assert isinstance(vocab, pd.DataFrame)
-    assert not vocab.empty
-    assert "platform_name" in vocab.columns
-    assert "02" in vocab["dfo_nafc_platform_code"].values
+class TestPlatformVocabulary:
+    def test_dfo_platform_load(self, platforms_vocab):
+        assert isinstance(platforms_vocab, pd.DataFrame)
+        assert not platforms_vocab.empty
+        assert "platform_name" in platforms_vocab.columns
+        assert "02" in platforms_vocab["dfo_nafc_platform_code"].values
+
+    @pytest.mark.parametrize(
+        "column",
+        [
+            "platform_name",
+            "platform_id",
+            "ices_platform_codes",
+            "wmo_platform_code",
+            "call_sign",
+            "sdn_platform_urn",
+            "dfo_nafc_platform_code",
+            "dfo_nafc_platform_name",
+        ],
+    )
+    def test_platform_unique_idendifiers(self, platforms_vocab, column):
+        duplicated = platforms_vocab.query(
+            f"{column}.notna() and {column}.duplicated(keep=False)"
+        )
+        assert (
+            duplicated.empty
+        ), f"{len(duplicated)} duplicated {column=} found in platform vocabulary: {duplicated[['platform_name',column]]}"
+
+    def test_sdn_urm_prefix(self, platforms_vocab):
+        sdn_prefix = platforms_vocab.query(
+            "sdn_platform_urn.str.startswith('SDN:C17::') == False and sdn_platform_urn.notna()"
+        )
+        assert (
+            sdn_prefix.empty
+        ), f"SDN URN prefix not found in platform vocabulary: {sdn_prefix[['platform_name','sdn_platform_urn']]}"
+
+    def test_matching_id_ices_sdn_codes(self, platforms_vocab):
+        mismatched_codes = platforms_vocab.query(
+            "ices_platform_codes != platform_id and (ices_platform_codes.notna() or platform_id.notna())"
+        )
+        assert (
+            mismatched_codes.empty
+        ), f"mismatched codes found: {mismatched_codes[['platform_name','platform_id','ices_platform_codes','sdn_platform_urn']]}"
+        mismatched_codes = platforms_vocab.query(
+            "sdn_platform_urn.str.replace('SDN:C17::','') != platform_id and (sdn_platform_urn.notna() or platform_id.notna())"
+        )
+        assert (
+            mismatched_codes.empty
+        ), f"mismatched codes found: {mismatched_codes[['platform_name','platform_id','ices_platform_codes','sdn_platform_urn']]}"
 
 
 def test_dfo_odf_vocabulary_load():
