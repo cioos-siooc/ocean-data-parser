7d54dc5dd6052ea8170ee0e7dd514976480ac938
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-05-08 23:02:07 -0400

    add test flags to ignore nerc tests by default

--- tests/test_vocabularies.py
+++ tests/test_vocabularies.py
@@ -2,12 +2,13 @@ import pandas as pd
 import pytest
 
 from ocean_data_parser.vocabularies import load
+from ocean_data_parser.metadata import nerc
 
 
-@pytest.fixture(scope="module")
-def platforms_vocab():
-    return load.dfo_platforms()
+nerc_vocabulary_test = pytest.mark.skipif("not config.getoption('nerc_vocab')")
 
+platforms_vocab = load.dfo_platforms()
+platforms_vocab["wmo_platform_code"] = platforms_vocab["wmo_platform_code"].apply(lambda x: int(x) if x else x)
 
 def test_amundsen_vocabulary_load():
     vocab = load.amundsen_vocabulary()
@@ -19,10 +20,8 @@ def test_seabird_vocabulary_load():
     vocab = load.seabird_vocabulary()
     assert vocab
     assert isinstance(vocab, dict)
-
-
 class TestPlatformVocabulary:
-    def test_dfo_platform_load(self, platforms_vocab):
+    def test_dfo_platform_load(self):
         assert isinstance(platforms_vocab, pd.DataFrame)
         assert not platforms_vocab.empty
         assert "platform_name" in platforms_vocab.columns
@@ -39,7 +38,7 @@ class TestPlatformVocabulary:
             "dfo_nafc_platform_name",
         ],
     )
-    def test_platform_unique_idendifiers(self, platforms_vocab, column):
+    def test_platform_unique_idendifiers(self, column):
         duplicated = platforms_vocab.query(
             f"{column}.notna() and {column}.duplicated(keep=False)"
         )
@@ -47,7 +46,7 @@ class TestPlatformVocabulary:
             duplicated.empty
         ), f"{len(duplicated)} duplicated {column=} found in platform vocabulary: {duplicated[['platform_name',column]]}"
 
-    def test_sdn_urm_prefix(self, platforms_vocab):
+    def test_sdn_urm_prefix(self):
         sdn_prefix = platforms_vocab.query(
             "sdn_platform_urn.str.startswith('SDN:C17::') == False and sdn_platform_urn.notna()"
         )
@@ -55,7 +54,7 @@ class TestPlatformVocabulary:
             sdn_prefix.empty
         ), f"SDN URN prefix not found in platform vocabulary: {sdn_prefix[['platform_name','sdn_platform_urn']]}"
 
-    def test_matching_id_ices_sdn_codes(self, platforms_vocab):
+    def test_matching_id_ices_sdn_codes(self):
         mismatched_codes = platforms_vocab.query(
             "ices_platform_codes != platform_id and (ices_platform_codes.notna() or platform_id.notna())"
         )
@@ -68,6 +67,20 @@ class TestPlatformVocabulary:
         assert (
             mismatched_codes.empty
         ), f"mismatched codes found: {mismatched_codes[['platform_name','platform_id','ices_platform_codes','sdn_platform_urn']]}"
+
+
+    @nerc_vocabulary_test
+    @pytest.mark.parametrize(
+        "id",
+        platforms_vocab['platform_id'].dropna().values,
+    )
+    def test_nerc_c17_to_platform(self, id):
+        attrs = ["platform_name", "platform_type", "country_of_origin", "platform_id", "ices_platform_code", "wmo_platform_code", "call_sign", "sdn_platform_urn"]
+
+        nerc_platform = nerc.get_platform_vocabulary(id)
+        nerc_platform = {attr: nerc_platform.get(attr) for attr in attrs if nerc_platform.get(attr) is not None}
+        local_platform = platforms_vocab.query(f"platform_id == '{id}'")[attrs].iloc[0].dropna().to_dict()
+        assert nerc_platform == local_platform
 
 
 def test_dfo_odf_vocabulary_load():
