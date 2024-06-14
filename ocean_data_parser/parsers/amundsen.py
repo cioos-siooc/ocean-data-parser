2f200f03ca9402a65ea675df33be527716738815
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-06-14 11:27:23 -0700

    specify amundsen timestamp date format (#98)
    * specify amundsen parser timestamp date format

--- ocean_data_parser/parsers/amundsen.py
+++ ocean_data_parser/parsers/amundsen.py
@@ -51,8 +51,14 @@ def _standardize_attribute_value(value: str, name: str = None):
     """
     if name in string_attributes or not isinstance(value, str):
         return value
-    elif re.match(r"\d\d-\w\w\w-\d\d\d\d \d\d\:\d\d\:\d\d", value):
-        return pd.to_datetime(value, utc=(name and "utc" in name))
+    elif re.fullmatch(r"\d\d-\w\w\w-\d\d\d\d \d\d\:\d\d\:\d\d", value):
+        return pd.to_datetime(
+            value, utc=(name and "utc" in name), format="%d-%b-%Y %H:%M:%S"
+        )
+    elif re.fullmatch(r"\d\d-\w\w\w-\d\d\d\d \d\d\:\d\d\:\d\d.\d", value):
+        return pd.to_datetime(
+            value, utc=(name and "utc" in name), format="%d-%b-%Y %H:%M:%S.%f"
+        )
     elif re.match(r"^-{0,1}\d+\.\d+$", value):
         return float(value)
     elif re.match(r"^-{0,1}\d+$", value):
