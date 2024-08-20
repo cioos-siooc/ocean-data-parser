c7e72c13e5a54deedf35e0a9c72e5d288f3f3191
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-08-20 13:13:45 -0400

    fix ruff

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,8 @@
 from pathlib import Path
-from ocean_data_parser import __version__
+
 import toml
+
+from ocean_data_parser import __version__
 
 
 def test_pyproject():
@@ -8,9 +10,8 @@ def test_pyproject():
     pyproject = toml.loads(Path("pyproject.toml").read_text())
     assert __version__ == pyproject["tool"]["poetry"]["version"]
 
+
 def test_changelog_version():
     """Test the CHANGELOG version"""
     changelog = Path("CHANGELOG.md").read_text()
     assert f"## `{__version__}`" in changelog
-
-
