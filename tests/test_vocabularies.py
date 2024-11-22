f786529cad98d5d3021950276e5fcf7c14012fe6
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2024-11-22 20:23:56 +0000

    add premilimary tests

--- tests/test_vocabularies.py
+++ tests/test_vocabularies.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytest
 
-from ocean_data_parser.metadata import nerc
+from ocean_data_parser.metadata import nerc, cf
 from ocean_data_parser.vocabularies import load
 
 nerc_vocabulary_test = pytest.mark.skipif("not config.getoption('nerc_vocab')")
@@ -12,16 +12,35 @@ platforms_vocab["wmo_platform_code"] = platforms_vocab["wmo_platform_code"].appl
 )
 
 
-def test_amundsen_vocabulary_load():
-    vocab = load.amundsen_vocabulary()
-    assert vocab
-    assert isinstance(vocab, dict)
+standard_names = cf.get_standard_names()
+nerc_p01 = nerc.get_vocabulary("P01")
+nerc_p06 = nerc.get_vocabulary("P06")
+nerc_p01['sdn_parameter_urn'] = nerc_p01['sdn_parameter_urn'].str.replace(
+    ':P01', ':P01::')
+nerc_p06['sdn_parameter_urn'] = nerc_p06['sdn_parameter_urn'].str.replace(
+    ':P06', ':P06::')
 
 
-def test_seabird_vocabulary_load():
-    vocab = load.seabird_vocabulary()
-    assert vocab
-    assert isinstance(vocab, dict)
+class TestVocabularyLoad:
+    def test_amundsen_vocabulary_load():
+        vocab = load.amundsen_vocabulary()
+        assert vocab
+        assert isinstance(vocab, dict)
+
+    def test_seabird_vocabulary_load():
+        vocab = load.seabird_vocabulary()
+        assert vocab
+        assert isinstance(vocab, dict)
+
+    def test_dfo_odf_vocabulary_load():
+        vocab = load.dfo_odf_vocabulary()
+        assert isinstance(vocab, pd.DataFrame)
+        assert not vocab.empty
+
+    def test_dfo_nafc_pfile_vocabulary_load():
+        vocab = load.dfo_nafc_p_file_vocabulary()
+        assert isinstance(vocab, pd.DataFrame)
+        assert not vocab.empty
 
 
 class TestPlatformVocabulary:
@@ -46,23 +65,27 @@ class TestPlatformVocabulary:
         duplicated = platforms_vocab.query(
             f"{column}.notna() and {column}.duplicated(keep=False)"
         )
-        assert duplicated.empty, f"{len(duplicated)} duplicated {column=} found in platform vocabulary: {duplicated[['platform_name',column]]}"
+        assert duplicated.empty, f"{len(duplicated)} duplicated {
+            column=} found in platform vocabulary: {duplicated[['platform_name', column]]}"
 
     def test_sdn_urm_prefix(self):
         sdn_prefix = platforms_vocab.query(
             "sdn_platform_urn.str.startswith('SDN:C17::') == False and sdn_platform_urn.notna()"
         )
-        assert sdn_prefix.empty, f"SDN URN prefix not found in platform vocabulary: {sdn_prefix[['platform_name','sdn_platform_urn']]}"
+        assert sdn_prefix.empty, f"SDN URN prefix not found in platform vocabulary: {
+            sdn_prefix[['platform_name', 'sdn_platform_urn']]}"
 
     def test_matching_id_ices_sdn_codes(self):
         mismatched_codes = platforms_vocab.query(
             "ices_platform_code != platform_id and (ices_platform_code.notna() or platform_id.notna())"
         )
-        assert mismatched_codes.empty, f"mismatched codes found: {mismatched_codes[['platform_name','platform_id','ices_platform_code','sdn_platform_urn']]}"
+        assert mismatched_codes.empty, f"mismatched codes found: {mismatched_codes[[
+            'platform_name', 'platform_id', 'ices_platform_code', 'sdn_platform_urn']]}"
         mismatched_codes = platforms_vocab.query(
             "sdn_platform_urn.str.replace('SDN:C17::','') != platform_id and (sdn_platform_urn.notna() or platform_id.notna())"
         )
-        assert mismatched_codes.empty, f"mismatched codes found: {mismatched_codes[['platform_name','platform_id','ices_platform_code','sdn_platform_urn']]}"
+        assert mismatched_codes.empty, f"mismatched codes found: {mismatched_codes[[
+            'platform_name', 'platform_id', 'ices_platform_code', 'sdn_platform_urn']]}"
 
     @nerc_vocabulary_test
     @pytest.mark.parametrize(
@@ -96,13 +119,87 @@ class TestPlatformVocabulary:
         assert nerc_platform == local_platform
 
 
-def test_dfo_odf_vocabulary_load():
-    vocab = load.dfo_odf_vocabulary()
-    assert isinstance(vocab, pd.DataFrame)
-    assert not vocab.empty
+vocabularies = [
+    load.amundsen_vocabulary_df,
+    load.seabird_vocabulary_df,
+    load.dfo_odf_vocabulary,
+    load.dfo_nafc_p_file_vocabulary,
+]
 
 
-def test_dfo_nafc_pfile_vocabulary_load():
-    vocab = load.dfo_nafc_p_file_vocabulary()
-    assert isinstance(vocab, pd.DataFrame)
-    assert not vocab.empty
+class TestVocabularies:
+
+    @pytest.mark.parametrize("vocabulary", vocabularies)
+    def test_standard_names(self, vocabulary):
+        """Test that the standard_name column is correctly formatted."""
+        vocab = vocabulary()
+        assert "standard_name" in vocab.columns
+        unknown_standard_names = vocab.query(
+            "standard_name.notna() and standard_name not in @standard_names['id']"
+        )
+        assert unknown_standard_names.empty, f"{len(unknown_standard_names)} unknown standard names found: {
+            unknown_standard_names.to_dict(orient='records')}"
+
+    @pytest.mark.parametrize("vocabulary", vocabularies)
+    def test_sdn_parameter_urn(self, vocabulary):
+        """Test that the sdn_parameter_urn column is correctly formatted."""
+        vocab = vocabulary()
+        if "sdn_parameter_urn" not in vocab.columns:
+            return
+        assert vocab["sdn_parameter_urn"].str.startswith("SDN:P01::").all()
+
+        unknown_urns = vocab.query(
+            "sdn_parameter_urn.notna() and sdn_parameter_urn not in @nerc_p01['sdn_parameter_urn']"
+        )
+        assert unknown_urns.empty, f"{len(unknown_urns)} unknown parameter URNs found: {
+            unknown_urns.to_dict(orient='records')}"
+
+    @pytest.mark.parametrize("vocabulary", vocabularies)
+    def test_sdn_parameter_name(self, vocabulary):
+        """Test that the sdn_parameter_name column is correctly formatted."""
+        vocab = vocabulary()
+        if "sdn_parameter_name" not in vocab.columns:
+            return
+        unknown_names = vocab.query(
+            "sdn_parameter_name.notna() and sdn_parameter_name not in @nerc_p01['prefLabel']"
+        )
+        assert unknown_names.empty, f"{len(unknown_names)} unknown parameter names found: {
+            unknown_names.to_dict(orient='records')}"
+
+        bad_urn_label_match = vocab[['sdn_parameter_name', 'sdn_parameter_urn']].notin(
+            nerc_p01[['prefLabel', 'sdn_parameter_urn']])
+        assert bad_urn_label_match.empty, f"Bad matches found: {
+            bad_urn_label_match.to_dict(orient='records')}"
+
+    @pytest.mark.parametrize("vocabulary", vocabularies)
+    def test_sdn_uom_urn(self, vocabulary):
+        """Test that the sdn_uom_urn column is correctly formatted."""
+        vocab = vocabulary()
+        if "sdn_uom_urn" not in vocab.columns:
+            return
+        not_p06 = ~ (vocab['sdn_uom_urn'].isna() |
+                     vocab['sdn_uom_urn'].str.startswith('SDN:P06::'))
+        assert not_p06.any(), f"UOM URNs do not start with 'SDN:P06::': {
+            vocab['sdn_uom_urn'][not_p06]}"
+        unknown_urns = vocab.query(
+            "sdn_uom_urn.notna() and sdn_uom_urn not in @nerc_p06['sdn_parameter_urn']"
+        )
+        assert unknown_urns.empty, f"{len(unknown_urns)} unknown UOM URNs found: {
+            unknown_urns.to_dict(orient='records')}"
+
+    @pytest.mark.parametrize("vocabulary", vocabularies)
+    def test_sdn_uom_name(self, vocabulary):
+        """Test that the sdn_uom_name column is correctly formatted."""
+        vocab = vocabulary()
+        if "sdn_uom_name" not in vocab.columns:
+            return
+        unknown_names = vocab.query(
+            "sdn_uom_name.notna() and sdn_uom_name not in @nerc_p06['prefLabel']"
+        )
+        assert unknown_names.empty, f"{len(unknown_names)} unknown UOM names found: {
+            unknown_names.to_dict(orient='records')}"
+
+        bad_matched_uom_label = vocab[['sdn_uom_name', 'sdn_uom_urn']].notin(
+            nerc_p06[['prefLabel', 'sdn_parameter_urn']])
+        assert bad_matched_uom_label.empty, f"Bad matches found: {
+            bad_matched_uom_label.to_dict(orient='records')}"
