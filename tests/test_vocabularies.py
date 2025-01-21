85ed0ee35bc860484398dd993ecee68e8028479f
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2025-01-21 10:51:31 -0500

    improve vocabularies tests

--- tests/test_vocabularies.py
+++ tests/test_vocabularies.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pytest
+import re
 
 from ocean_data_parser.metadata import nerc, cf
 from ocean_data_parser.vocabularies import load
@@ -125,100 +126,138 @@ vocabularies = [
     load.dfo_odf_vocabulary,
     load.dfo_nafc_p_file_vocabulary,
 ]
+ACCEPTED_URN_VARIANCE = [
+    "XX",
+    "X",
+    "1",
+    "01",
+]
 
 
+def is_valid_urn(urn, urns):
+    if pd.isna(urn):
+        return True
+    if urn in urns:
+        return True
+    if not re.search(r"\d+$", urn):
+        return False
+    for variance in ACCEPTED_URN_VARIANCE:
+        if re.sub(r"\d+$", variance, urn) in urns:
+            return True
+    return False
+
+def get_matching_urn(urn, urns):
+    if pd.isna(urn):
+        return
+    if urn in urns:
+        return urn
+    if not re.search(r"\d+$", urn):
+        return None
+    for variance in ACCEPTED_URN_VARIANCE:
+        if re.sub(r"\d+$", variance, urn) in urns:
+            return re.sub(r"\d+$", variance, urn)
+    return None
+
+
+@pytest.mark.parametrize("load_vocabulary", vocabularies)
 class TestVocabularies:
-    @pytest.mark.parametrize("vocabulary", vocabularies)
+    @pytest.fixture(autouse=True)
+    def vocabulary(self, load_vocabulary):
+        return load_vocabulary()
+
     def test_standard_names(self, vocabulary):
         """Test that the standard_name column is correctly formatted."""
-        vocab = vocabulary()
-        assert "standard_name" in vocab.columns
-        unknown_standard_names = vocab.query(
+        assert "standard_name" in vocabulary.columns
+        unknown_standard_names = vocabulary.query(
             "standard_name.notna() and standard_name not in @standard_names['id']"
         )
         assert unknown_standard_names.empty, (
             f"{len(unknown_standard_names)} unknown standard names found: {unknown_standard_names.to_dict(orient='records')}"
         )
 
-    @pytest.mark.parametrize("vocabulary", vocabularies)
     def test_sdn_parameter_urn(self, vocabulary):
         """Test that the sdn_parameter_urn column is correctly formatted."""
-        vocab = vocabulary()
-        if "sdn_parameter_urn" not in vocab.columns:
+        if "sdn_parameter_urn" not in vocabulary.columns:
             return
 
         not_p01 = ~(
-            vocab["sdn_parameter_urn"].isna()
-            | vocab["sdn_parameter_urn"].str.startswith("SDN:P01::")
+            vocabulary["sdn_parameter_urn"].isna()
+            | vocabulary["sdn_parameter_urn"].str.startswith("SDN:P01::")
         )
         assert not not_p01.any(), (
-            f"Parameter URNs do not start with 'SDN:P01::': {vocab['sdn_parameter_urn'][not_p01]}"
+            f"Parameter URNs do not start with 'SDN:P01::': {vocabulary['sdn_parameter_urn'][not_p01]}"
         )
 
-        unknown_urns = vocab.query(
-            "sdn_parameter_urn.notna() and sdn_parameter_urn not in @nerc_p01['sdn_parameter_urn']"
+        valid_p01_urn = vocabulary["sdn_parameter_urn"].apply(
+            lambda urn: is_valid_urn(urn, nerc_p01["sdn_parameter_urn"].to_list())
         )
-        assert unknown_urns.empty, (
+        unknown_urns = vocabulary.loc[~valid_p01_urn]
+        assert valid_p01_urn.all(), (
             f"{len(unknown_urns)} unknown parameter URNs found [{unknown_urns['sdn_parameter_urn'].unique()}]: {unknown_urns.to_dict(orient='records')}"
         )
+        vocabulary["matched_urn"] = vocabulary["sdn_parameter_urn"].apply(
+            lambda urn: get_matching_urn(urn, nerc_p01["sdn_parameter_urn"].to_list())
+        )
 
-    @pytest.mark.parametrize("vocabulary", vocabularies)
+        comparison = vocabulary.merge(
+            nerc_p01[["sdn_parameter_urn", "sdn_parameter_name"]],
+            left_on=["sdn_parameter_name", "sdn_parameter_urn"],
+            right_on=["sdn_parameter_name", "matched_urn"],
+            how="left",
+            indicator=True,
+        ).dropna(subset=["sdn_parameter_name", "sdn_parameter_urn"], how="all")
+        mismatches = comparison[comparison["_merge"] == "left_only"]
+        assert mismatches.empty, (
+            f"Bad {len(mismatches)} mismatches found {mismatches['sdn_parameter_urn'].unique()}: {mismatches.to_dict(orient='records')}"
+        )
+
     def test_sdn_parameter_name(self, vocabulary):
-        """Test that the sdn_parameter_name column is correctly formatted."""
-        vocab = vocabulary()
-        if "sdn_parameter_name" not in vocab.columns:
+        """Test that the sdn_parameter_name column is correctly formatted.
+
+        We accept variances of existing URNs ending with XX, X, 1 or 01.
+        As long as this variance is numerical variance only.
+        """
+        if "sdn_parameter_name" not in vocabulary.columns:
             return
-        unknown_names = vocab.query(
+        unknown_names = vocabulary.query(
             "sdn_parameter_name.notna() and sdn_parameter_name not in @nerc_p01['sdn_parameter_name']"
         )
         assert unknown_names.empty, (
             f"{len(unknown_names)} unknown parameter names found [{unknown_names['sdn_parameter_name'].unique()}]: {unknown_names.to_dict(orient='records')}"
         )
 
-        comparison = vocab.merge(
-            nerc_p01[["sdn_parameter_urn", "sdn_parameter_name"]],
-            on=["sdn_parameter_name", "sdn_parameter_urn"],
-            how="left",
-            indicator=True,
-        ).dropna(subset=["sdn_parameter_name", "sdn_parameter_urn"], how="all")
-        mismatches = comparison[comparison["_merge"] == "left_only"]
-        assert mismatches.empty, (
-            f"Bad {len(mismatches)} mismatches found: {mismatches.to_dict(orient='records')}"
-        )
 
-    @pytest.mark.parametrize("vocabulary", vocabularies)
     def test_sdn_uom_urn(self, vocabulary):
         """Test that the sdn_uom_urn column is correctly formatted."""
-        vocab = vocabulary()
-        if "sdn_uom_urn" not in vocab.columns:
+        if "sdn_uom_urn" not in vocabulary.columns:
             return
         not_p06 = ~(
-            vocab["sdn_uom_urn"].isna()
-            | vocab["sdn_uom_urn"].str.startswith("SDN:P06::")
+            vocabulary["sdn_uom_urn"].isna()
+            | vocabulary["sdn_uom_urn"].str.startswith("SDN:P06::")
         )
         assert not not_p06.any(), (
-            f"UOM URNs do not start with 'SDN:P06::': {vocab['sdn_uom_urn'][not_p06]}"
+            f"UOM URNs do not start with 'SDN:P06::': {vocabulary['sdn_uom_urn'][not_p06]}"
         )
-        unknown_urns = vocab.query(
-            "sdn_uom_urn.notna() and sdn_uom_urn not in @nerc_p06['sdn_parameter_urn']"
+        unknown_urns = vocabulary.query(
+            "sdn_uom_urn.notna() and sdn_uom_urn not in @nerc_p06['sdn_uom_urn']"
         )
         assert unknown_urns.empty, (
             f"{len(unknown_urns)} unknown UOM URNs found: {unknown_urns.to_dict(orient='records')}"
         )
 
-    @pytest.mark.parametrize("vocabulary", vocabularies)
+    # @pytest.mark.parametrize("vocabulary", vocabularies)
     def test_sdn_uom_name(self, vocabulary):
         """Test that the sdn_uom_name column is correctly formatted."""
-        vocab = vocabulary()
-
-        unknown_names = vocab.query(
+        if "sdn_uom_name" not in vocabulary.columns:
+            return
+        unknown_names = vocabulary.query(
             "sdn_uom_name.notna() and sdn_uom_name not in @nerc_p06['sdn_uom_name']"
         )
         assert unknown_names.empty, (
             f"{len(unknown_names)} unknown UOM names found [{set(unknown_names.sdn_uom_name.to_list())}]: {unknown_names.to_records()}"
         )
 
-        comparison = vocab.merge(
+        comparison = vocabulary.merge(
             nerc_p06[["sdn_uom_urn", "sdn_uom_name"]],
             on=["sdn_uom_name", "sdn_uom_urn"],
             how="left",
