b8d2a2b1eba03b0e5db4a2a08e14805515a9fe5e
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2026-05-26 14:56:57 -0400

    feat: integrate standardize_dataset function in NMEA, Star-Oddi, and Sunburst parsers; update test to ignore version history

--- tests/test_reference_netcdf.py
+++ tests/test_reference_netcdf.py
@@ -108,13 +108,20 @@ def compare_test_to_reference_netcdf(
     # Add placeholders to specific fields in attributes
     ignore_from_attr(
         "history",
+        r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)? "
+        r"Generated with ocean_data_parser v\d+\.\d+\.\d+\n?",
+        "",
+    )
+    ignore_from_attr(
+        "history",
         r"cioos_data_trasform.odf_transform V \d+\.\d+\.\d+|"
         r"ocean_data_parser V \d+\.\d+\.\d+",
-        r"Generated with ocean_data_parser v\d+\.\d+\.\d+",
         "package_name_version",
     )
     ignore_from_attr(
-        "history", r"\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.*\d*Z", "TIMESTAMP"
+        "history",
+        r"\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?Z?",
+        "TIMESTAMP",
     )
     ignore_from_attr("source", ".*", "source")
 
